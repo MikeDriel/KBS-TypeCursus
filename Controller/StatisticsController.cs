0716a6534dac76f0fd6b2c072f721ae3839864b0
MikeDriel <94164663+MikeDriel@users.noreply.github.com>
2022-12-07 17:34:06 +0100

    fix

--- Controller/StatisticsController.cs
+++ Controller/StatisticsController.cs
@@ -7,10 +7,10 @@ using System.Xml.Serialization;
 
 namespace Controller
 {
-	public class StatisticsController
-	{
+    public class StatisticsController
+    {
         public int NumberOfMistakes { get; private set; }
-        public int NumberCorrect{ get; private set; }
+        public int NumberCorrect { get; private set; }
         private bool _hasBeenWrong;
         public DateTime CurrentTime { get; set; }
         public int TimeLeft { get; set; }
@@ -19,79 +19,78 @@ namespace Controller
         public event EventHandler<LiveStatisticsEventArgs> LiveStatisticsEvent;
 
         public StatisticsController()
-		{
-			CurrentTime = new DateTime();
-			NumberOfMistakes = 0; 
-			NumberCorrect = 0;
+        {
+            CurrentTime = new DateTime();
+            NumberOfMistakes = 0;
+            NumberCorrect = 0;
             _timer.Elapsed += OnTimedEvent;
             _hasBeenWrong = false;
         }
 
         public void StartTimer()
         {
-			if (!_timer.Enabled)
-			{
+            if (!_timer.Enabled)
+            {
                 _timer.Start();
             }
         }
 
-		public void ResetTimeLeft()
-		{
+        public void ResetTimeLeft()
+        {
             TimeLeft = _maxTimePerKey;
         }
 
         public string GetStatistics()
-		{
-			double PercentGood;
-			if (NumberCorrect == 0)
-			{
-				PercentGood = 0;
-			}
-			else if (NumberOfMistakes == 0)
-			{
-				PercentGood = 100;
-			}
-			else
-			{
-				PercentGood = ((double)NumberCorrect / ((double)NumberCorrect + (double)NumberOfMistakes)) * 100;
-				PercentGood = Math.Round(PercentGood, 1);
-			}
-			return $" {NumberOfMistakes} fout \r\n {PercentGood}% goed \r\n {CurrentTime.ToString("mm:ss")}";
-		}
+        {
+            double PercentGood;
+            if (NumberCorrect == 0)
+            {
+                PercentGood = 0;
+            }
+            else if (NumberOfMistakes == 0)
+            {
+                PercentGood = 100;
+            }
+            else
+            {
+                PercentGood = ((double)NumberCorrect / ((double)NumberCorrect + (double)NumberOfMistakes)) * 100;
+                PercentGood = Math.Round(PercentGood, 1);
+            }
+
+            return $" {NumberOfMistakes} fout \r\n {PercentGood}% goed \r\n {CurrentTime.ToString("mm:ss")}";
+        }
 
         private void OnTimedEvent(object sender, EventArgs e)
         {
             if (TimeLeft == 0)
             {
-	            WrongAnswer();
-	            TimeLeft = _maxTimePerKey;
-	            }
-            LiveStatisticsEvent?.Invoke(this, new LiveStatisticsEventArgs());            
+                WrongAnswer();
+                TimeLeft = _maxTimePerKey;
+            }
+
+            LiveStatisticsEvent?.Invoke(this, new LiveStatisticsEventArgs());
             TimeLeft--;
             CurrentTime = CurrentTime.AddSeconds(1);
         }
 
         public void WrongAnswer()
         {
-	        if (!_hasBeenWrong)
-	        {
-		        NumberOfMistakes++;
-		        _hasBeenWrong = true;
-	        }
+            if (!_hasBeenWrong)
+            {
+                NumberOfMistakes++;
+                _hasBeenWrong = true;
+            }
         }
-        
+
         public void RightAnswer()
-		{
-			NumberCorrect++;
-			_hasBeenWrong = false;
-		}
+        {
+            NumberCorrect++;
+            _hasBeenWrong = false;
+        }
     }
 
-    //EVENT FOR LIVE STATISCTICS UPDATE
+    //EVENT FOR LIVE STATISTICS UPDATE
     public class LiveStatisticsEventArgs : EventArgs
     {
-        public LiveStatisticsEventArgs()
-        {
-        }
     }
 }
