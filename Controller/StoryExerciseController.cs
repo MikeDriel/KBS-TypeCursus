286d60490efb78a1123ec8bcbce5e22be0c5eb07
BasHof <bas.hofstede@windesheim.nl>
2022-12-08 14:12:03 +0100

    still working

--- Controller/StoryExerciseController.cs
+++ Controller/StoryExerciseController.cs
@@ -9,67 +9,87 @@ namespace Controller
     public class StoryExerciseController
     {
         public event EventHandler<ExerciseEventArgs> ExerciseEvent;
-        public int _typingIndex { get; set; } //the index of the current letter that is being typed 
+        public int TypingIndex { get; set; } //the index of the current letter that is being typed 
         public ExerciseController exerciseController;
         public string Story = "het leven is een tekening die je inkleurt. Op 5 december komt Sinterklaas met zwarte Piet naar jouw schoorsteen toe.";
-        public List<char> _charListBackCorrect;
-        public List<char> _charListBack;
-        public List<char> _charListFront;
+        public List<char> _charListCorrect;
+        public List<char> _charListTyped;
         public char _currentChar;
-        
+
 
         public StoryExerciseController()
         {
             exerciseController = new ExerciseController(2);
-            _charListBackCorrect = new List<char>();
-            _charListBack = new List<char>();
-            _charListFront = new List<char>();
+            _charListCorrect = new List<char>();
+            _charListTyped = new List<char>();
             _currentChar = exerciseController.CurrentChar;
-            
-            
+            TypingIndex = 0;
         }
 
         public void SetCharListBack()
         {
             foreach (char character in Story)
             {
-                _charListBackCorrect.Add(character);
+                _charListCorrect.Add(character);
             }
-            _charListBack = _charListBackCorrect;
+            //_charListTyped = _charListCorrect;
         }
 
         public void CheckIfLetterIsCorrectStory()
         {
-            {//checks if list isnt empty
-                if (_charListBackCorrect.Count >= 1)
+            //checks if list isnt empty
+
+            if (_currentChar == _charListCorrect[TypingIndex])
+            {
+                _charListTyped.Add(_currentChar);
+               
+                if (TypingIndex == _charListCorrect.Count)
                 {
-                    //checks if the last keypress is equal to the first letter in the queue
-                    if (_charListBackCorrect[_typingIndex] == _currentChar)
-                    {
-                        //if it is, add letter to _charListFront
-                        _charListFront.Add(_currentChar);
-                        //flag for correct letter (color)
-
-
-                        if (_charListFront.Count == _charListBackCorrect.Count)
-                        {
-                            ExerciseEvent?.Invoke(this, new ExerciseEventArgs(true, true));
-                        }
-                        else
-                        {
-                            ExerciseEvent?.Invoke(this, new ExerciseEventArgs(true, false));
-                        }
-                        _typingIndex++;
-                    }
-                    else
-                    {
-                        //_charListFront.Add(_currentChar);
-                        //flag for wrong letter (color)
-                        ExerciseEvent?.Invoke(this, new ExerciseEventArgs(false, false));
-                        _typingIndex++;
-                    }
+                    ExerciseEvent?.Invoke(this, new ExerciseEventArgs(true, true));
+                    
+                }
+                else
+                {
+                    ExerciseEvent?.Invoke(this, new ExerciseEventArgs(true, false));
+                    TypingIndex++;
                 }
             }
+            else
+            {
+                ExerciseEvent?.Invoke(this, new ExerciseEventArgs(false, false));
+            }
+           
+
+
+            //if (_charListCorrect.Count >= 1)
+            //{
+            //    //checks if the last keypress is equal to the first letter in the queue
+            //    if (_charListCorrect[_typingIndex] == _currentChar)
+            //    {
+            //        //if it is, add letter to _charListFront
+            //        //_charListFront.Add(_currentChar);
+            //        //flag for correct letter (color)
+
+
+            //        if (_charListFront.Count == _charListCorrect.Count)
+            //        {
+            //            ExerciseEvent?.Invoke(this, new ExerciseEventArgs(true, true));
+            //        }
+            //        else
+            //        {
+            //            ExerciseEvent?.Invoke(this, new ExerciseEventArgs(true, false));
+            //        }
+            //        _typingIndex++;
+            //    }
+            //    else
+            //    {
+            //        //_charListFront.Add(_currentChar);
+            //        //flag for wrong letter (color)
+            //        ExerciseEvent?.Invoke(this, new ExerciseEventArgs(false, false));
+            //        _typingIndex++;
+            //    }
+            //}
+
         }
 
 
--- WPF_Visualize/Views Exercises/StoryExercise.xaml.cs
+++ WPF_Visualize/Views Exercises/StoryExercise.xaml.cs
@@ -1,4 +1,6 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
 using System.Text;
 using System.Windows;
 using System.Windows.Controls;
@@ -21,12 +23,10 @@ namespace WPF_Visualize
         Controller.ExerciseController _controller;
         Controller.StoryExerciseController _storyController;
         Controller.StatisticsController _statisticsController = new();
-        
+
         Rectangle _rectangleLetterTyped = new Rectangle { Width = 33, Height = 33, Fill = Brushes.Gray, Opacity = 0.75 }; //Makes rectangle
         Rectangle _rectangleLetterToType = new Rectangle { Width = 33, Height = 33, Fill = Brushes.Gray, Opacity = 0.75 }; //Makes rectangle
 
-    
-       
 
 
 
@@ -34,7 +34,9 @@ namespace WPF_Visualize
 
 
 
-    public StringBuilder _sb = new StringBuilder();
+
+
+        public StringBuilder _sb = new StringBuilder();
         public StoryExercise()
         {
             InitializeComponent();
@@ -45,9 +47,15 @@ namespace WPF_Visualize
 
             _storyController.SetCharListBack();
             ChangeTextOnScreen();
+
+            //rectangles for the letters
             KeyboardCanvas.Children.Add(_rectangleLetterToType); //adds rectangle on screen
             KeyboardCanvas.Children.Add(_rectangleLetterTyped);
             _rectangleLetterTyped.Visibility = Visibility.Hidden;
+
+            //Add the text to the textbox
+            RichTextBoxStory.Document.Blocks.Clear();
+            RichTextBoxStory.Document.Blocks.Add(new Paragraph(new Run(_storyController.Story)));
         }
 
         //Connects events to the button 
@@ -62,29 +70,31 @@ namespace WPF_Visualize
         private void HandleKeyPress(object sender, KeyEventArgs e)
         {
 
-            if (e.Key.ToString().Equals("Space"))
-            {
-                _storyController._currentChar = ' ';
-            }
-            else 
-            {
-                if (e.Key == Key.LeftShift || e.Key == Key.RightShift)
-                {
-                    if(e.Key.ToString().Length == 1)
-                    {
-                        _storyController._currentChar = e.Key.ToString().ToUpper()[0];
-                    }
-                    
-                }
-                else
-                {
-                    if (e.Key.ToString().Length == 1)
-                    {
-                        _storyController._currentChar = e.Key.ToString().ToLower()[0];
-                    } 
-                }
-                
-            }
+            //if (e.Key.ToString().Equals("Space"))
+            //{
+            //    _storyController._currentChar = ' ';
+            //}
+            //else 
+            //{
+            //    if (e.Key == Key.LeftShift || e.Key == Key.RightShift)
+            //    {
+            //        if(e.Key.ToString().Length == 1)
+            //        {
+            //            _storyController._currentChar = e.Key.ToString().ToUpper()[0];
+            //        }
+
+            //    }
+            //    else
+            //    {
+            //        if (e.Key.ToString().Length == 1)
+            //        {
+            //            _storyController._currentChar = e.Key.ToString().ToLower()[0];
+            //        } 
+            //    }
+
+            //}
+
+            _storyController._currentChar = e.Key.ToString().ToLower()[0];
             _storyController.CheckIfLetterIsCorrectStory();
             _statisticsController.ResetTimeLeft();
             _statisticsController.StartTimer();
@@ -94,46 +104,41 @@ namespace WPF_Visualize
         private void HandleKeyUp(object sender, KeyEventArgs e)
         {
 
-            
+
         }
 
 
         //updates values on view
         private void ChangeTextOnScreen()
         {
-            StoryTextBoxBack.SelectAll();
-            StoryTextBoxBack.Selection.Text = "";
-            foreach (var item in _storyController._charListBack)
-            {
-                StoryTextBoxBack.AppendText(item.ToString());
-            }
+            //RichTextBoxStory.SelectAll();
+            //RichTextBoxStory.Selection.Text = "";
+            //foreach (var item in _storyController._charListTyped)
+            //{
+            //    RichTextBoxStory.AppendText(item.ToString());
+            //}
 
-            StoryTextBoxFront.SelectAll();
-            StoryTextBoxFront.Selection.Text = "";
-            foreach (var item in _storyController._charListFront)
-            {
-                StoryTextBoxFront.AppendText(item.ToString());
-            }
+            ////StoryTextBoxFront.SelectAll();
+            ////StoryTextBoxFront.Selection.Text = "";
+            ////foreach (var item in _storyController._charListFront)
+            ////{
+            ////    StoryTextBoxFront.AppendText(item.ToString());
+            ////}
 
-
-            
-
-
-
-            //StoryTextBoxBack.AppendText(_charListBack.ToString());
-            //StoryTextBoxFront.AppendText(_charListFront.ToString());
-            SetLiveStatistics(this, null);
+            ////RichTextBoxStory.AppendText(_charListBack.ToString());
+            ////StoryTextBoxFront.AppendText(_charListFront.ToString());
+            //SetLiveStatistics(this, null);
         }
 
         //updates statistics on view
         private void SetLiveStatistics(object sender, LiveStatisticsEventArgs e)
         {
-            this.Dispatcher?.Invoke(() =>
-            {
-                string statistics = _statisticsController.GetStatistics();
-                LiveStatisticsScreen.Content = statistics;
-                TimeLeftLabel.Content = _statisticsController.TimeLeft;
-            });
+            //this.Dispatcher?.Invoke(() =>
+            //{
+            //    string statistics = _statisticsController.GetStatistics();
+            //    LiveStatisticsScreen.Content = statistics;
+            //    TimeLeftLabel.Content = _statisticsController.TimeLeft;
+            //});
         }
 
         //The back button top left
@@ -151,17 +156,13 @@ namespace WPF_Visualize
             _storyController._currentChar = '.';
         }
 
-   
-
-   
-
-
-
         //Methods for events to fire
         private void MistakeMade()
         {
             //if the letter is wrong, add a mistake and update the screen
             _statisticsController.NumberOfMistakes++;
+            ColorTheKs(false);
+
         }
 
         private void ExerciseFinished()
@@ -175,10 +176,63 @@ namespace WPF_Visualize
 
         private void CorrectAnswer()
         {
-
             _statisticsController.NumberCorrect++;
+            ColorTheKs(true);
+        }
 
-            //Makes the letter black again
+        //private void ColorTheKs(bool correct)
+        //{
+        //    int index = _storyController.TypingIndex;
+        //    indexLabel.Content = index;
+        //    //// Get the starting position of the entire text
+        //    //TextPointer startPosition = RichTextBoxStory.Document.ContentStart;
+
+        //    //// Get the position at a specific offset (character position)
+        //    //TextPointer position = startPosition.GetPositionAtOffset(index);
+
+        //    //// Get the ending position by moving forward 10 characters
+        //    //TextPointer endPosition = position.GetPositionAtOffset(index);
+
+        //    //// Create a new TextRange object that represents the text
+        //    //TextRange selectedText = new TextRange(position, endPosition);
+
+        //    //// Re-select the text
+        //    //RichTextBoxStory.Selection.Select(selectedText.Start, selectedText.End);
+
+
+
+        //    TextPointer start = RichTextBoxStory.Document.ContentStart.GetPositionAtOffset(index);
+        //    TextPointer end = start.GetPositionAtOffset(1);
+        //    TextRange range = new TextRange(start, end);
+
+        //    if (correct)
+        //    {
+        //        range.ApplyPropertyValue(TextElement.ForegroundProperty, Brushes.Green);
+        //    }
+        //    else
+        //    {
+        //        range.ApplyPropertyValue(TextElement.ForegroundProperty, Brushes.Red);
+        //    }
+
+        //}
+
+        void ColorTheKs(bool correct)
+        {
+            // Set the color of the character at the specified index
+            int index = _storyController.TypingIndex;
+            TextPointer start = RichTextBoxStory.Document.ContentStart.GetPositionAtOffset(index);
+            TextPointer end = start.GetPositionAtOffset(1);
+            TextRange range = new TextRange(start, end);
+
+            // Set the color to red or green depending on the value of the 'correct' parameter
+            if (correct)
+            {
+                range.ApplyPropertyValue(TextElement.ForegroundProperty, Brushes.Green);
+            }
+            else
+            {
+                range.ApplyPropertyValue(TextElement.ForegroundProperty, Brushes.Red);
+            }
         }
 
         //EVENTS
@@ -200,8 +254,8 @@ namespace WPF_Visualize
             ChangeTextOnScreen();
         }
 
-     
 
-     
+
+
     }
 }
