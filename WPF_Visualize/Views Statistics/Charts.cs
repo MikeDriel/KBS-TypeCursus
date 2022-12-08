d1dc8fa91a5370ef2779b7a0d21a005f8c14ba3e
MikeDriel <94164663+MikeDriel@users.noreply.github.com>
2022-12-08 12:40:58 +0100

    Fix van de pijn

--- WPF_Visualize/Views Statistics/Charts.cs
+++ WPF_Visualize/Views Statistics/Charts.cs
@@ -11,26 +11,23 @@ using System.Security.Cryptography.Xml;
 
 namespace WPF_Visualize.Views_Statistics
 {
-    public class Charts
-    {
-        public Charts()
-        {
-            Data = new List<DataPoint>();
-            Data.Add(new DataPoint(0, 0));
+	public class Charts
+	{
+		public Charts()
+		{
+			Data = new List<DataPoint>();
+			Data.Add(new DataPoint(0, 0));
 
-            for (int i = 1; i < Exercise._statisticsController.CharactersPerSecond.Count; i++)
-            {
-                Data.Add(new DataPoint(i, Exercise._statisticsController.CharactersPerSecond[i]));
-                this.Stats = new PlotModel { Title = "Tekens per seconde" };
-                this.Stats.Series.Add(new LineSeries { ItemsSource = Data, Title = "Series 1" });
+			for (int i = 1; i < Exercise._statisticsController.CharactersPerSecond.Count; i++)
+			{
+				Data.Add(new DataPoint(i, Exercise._statisticsController.CharactersPerSecond[i]));
+				this.Stats = new PlotModel { Title = "Tekens per seconde" };
+				this.Stats.Series.Add(new LineSeries { ItemsSource = Data, Title = "Series 1" });
 
-            }
-        }
+			}
+		}
 
-        public IList<DataPoint> Data { get; private set; }
-        public PlotModel Stats { get; private set; }
-
-
-
-    } 
+		public IList<DataPoint> Data { get; private set; }
+		public PlotModel Stats { get; private set; }
+	}
 }
