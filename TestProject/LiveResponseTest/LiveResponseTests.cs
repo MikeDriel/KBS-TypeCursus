0e25c33dd35220f0a8dd6a1cf2da0c54cf17835b
Doede2021 <doede.dijkstra@outlook.com>
2022-12-12 15:51:26 +0100

    LiveResponseTest

---
+++ TestProject/LiveResponseTest/LiveResponseTests.cs
@@ -0,0 +1,32 @@
+﻿using Controller;
+using Model;
+using System;
+
+namespace TestProject.DataBaseTest
+{
+    [TestFixture]
+    public class LiveResponseTests
+    {
+        StatisticsController _statisticsController;
+
+
+        [SetUp]
+        public void Setup()
+        {
+            _statisticsController = new StatisticsController(5);
+        }
+
+        [Test]
+        //test for live response, testing if the if statements of the GetStatistics method works as expected
+        public void GetStatistics_StandardVariables_ResultsEqual()
+        {       
+            string result = _statisticsController.GetStatistics();
+            string expectedResult = $" 0 fout \r\n 0% goed \r\n {_statisticsController.CurrentTime.ToString("mm:ss")}";
+            Assert.AreEqual(result, expectedResult);
+        }
+
+
+
+
+    }
+}
