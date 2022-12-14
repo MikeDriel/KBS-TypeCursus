4ad3f4d493da3a46ffc88f618dcbc5ede97e6f7d
BasHof <bas.hofstede@windesheim.nl>
2022-12-14 15:00:47 +0100

    Begin of teacherpage and styling

---
+++ WPF_Visualize/Views Navigate/ClassSelect.xaml.cs
@@ -0,0 +1,47 @@
+﻿using Controller;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using WPF_Visualize.ViewLogic;
+using WPF_Visualize.ViewLogin;
+
+namespace WPF_Visualize.Views_Navigate
+{
+    /// <summary>
+    /// Interaction logic for ClassSelect.xaml
+    /// </summary>
+    public partial class ClassSelect : UserControl
+    {
+        public ClassSelect()
+        {
+            InitializeComponent();
+        }
+        private void OnLogOut(object sender, RoutedEventArgs e)
+        {
+            LoginController.LogOut();
+            UserControlController.MainWindowChange(this, new LoginChoose());
+        }
+
+        // Close the application
+        private void OnAfsluiten(object sender, RoutedEventArgs e)
+        {
+            Application.Current.Shutdown();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            UserControlController.MainWindowChange(this, new TeacherMain());
+        }
+    }
+}
--- WPF_Visualize/Views Navigate/TeacherMain.xaml.cs
+++ WPF_Visualize/Views Navigate/TeacherMain.xaml.cs
@@ -3,6 +3,7 @@ using System.Windows.Controls;
 using Controller;
 using WPF_Visualize.ViewLogic;
 using WPF_Visualize.ViewLogin;
+using WPF_Visualize.Views_Statistics;
 
 namespace WPF_Visualize.Views_Navigate;
 
@@ -16,9 +17,26 @@ public partial class TeacherMain : UserControl
         InitializeComponent();
     }
 
+    private void OnClassStatistics(object sender, RoutedEventArgs e)
+    {
+        UserControlController.MainWindowChange(this, new ClassStatistics());
+    }
+
+    private void OnClassSettings(object sender, RoutedEventArgs e)
+    {
+        UserControlController.MainWindowChange(this, new ClassSettings());
+    }
+
+
     private void OnLogOut(object sender, RoutedEventArgs e)
     {
         LoginController.LogOut();
         UserControlController.MainWindowChange(this, new LoginChoose());
+    }
+
+    // Close the application
+    private void OnAfsluiten(object sender, RoutedEventArgs e)
+    {
+        Application.Current.Shutdown();
     }
 }
